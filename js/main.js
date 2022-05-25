d2cea5cc9b8b518b3caecd3ea7fd4c4044dcd121
Hunter K <huntertkoehn@gmail.com>
2022-05-25 16:15:39 -0500

    initialize github repo

---
+++ js/main.js
@@ -0,0 +1,32 @@
+document.querySelector('button').addEventListener('click', getFetch)
+document.querySelector('starBtn').addEventListener('click', popUp)
+
+function getFetch() {
+  const choice = document.querySelector('input').value
+  console.log(choice)
+
+  const url = `https://api.nasa.gov/planetary/apod?api_key=v11TRPhmT4MF0b2nrr9ffdzpKcw9TXBk3NXHhfY4&date=${choice}`
+
+  fetch(url)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data)
+        if( data.media_type === 'image' ){
+          document.querySelector('img').src = data.hdurl
+        }
+        else if(data.media_type === 'video'){
+          document.querySelector('iframe').src = data.url
+        }
+       
+        document.querySelector('h3').innerText = data.explanation
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
+}
+
+//todo: create the popUp function
+function popUp() {
+
+}
+  
