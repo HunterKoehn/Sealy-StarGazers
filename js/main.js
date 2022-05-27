e7cb3982f44e052d4ada874cb3d4031bd99bee53
Hunter K <huntertkoehn@gmail.com>
2022-05-27 13:08:37 -0500

    tried some things that didnt work, got half of it though

--- js/main.js
+++ js/main.js
@@ -31,14 +31,37 @@ function getFetch() {
 }
 
 //todo: create function to save whatever image is currently being shown to the user.
-
+// need to figure out how to send the image to computer instead of the json...
 
 //code here
 
-// mediaDL = document.getElementById('downloadBtn')
 
-// mediaDL.addEventListener('click', function getMedia () { 
-  
-//   localStorage.setItem('media', document.querySelector('img').src);
-//   localStorage.getItem('media');
-// });
+// Code to test the onclick
+// function getMedia() {
+//   alert('You clicked me');
+// }
+
+
+// download initiates with this code!! V
+
+// function getMedia(){
+//   const choice = document.getElementById('pho').getAttribute('src');
+//   console.log(choice)
+//   let name = 'untitled'; 
+//   const url = `https://api.nasa.gov/planetary/apod?api_key=v11TRPhmT4MF0b2nrr9ffdzpKcw9TXBk3NXHhfY4&date=${choice}`
+
+//   fetch(url)
+//     .then(resp => resp.blob())
+//     .then(blob => {
+//         const url = window.URL.createObjectURL(blob);
+//         const a = document.createElement('a');
+//         a.style.display = 'none';
+//         a.href = url;
+//         // the filename you want
+//         a.download = name;
+//         document.body.appendChild(a);
+//         a.click();
+//         window.URL.revokeObjectURL(url);
+//     })
+//     .catch(() => alert('An error sorry'));
+// }
